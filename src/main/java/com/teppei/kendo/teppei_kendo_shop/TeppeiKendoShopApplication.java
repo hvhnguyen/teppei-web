5d2b13a69a3b1aa20f5c70e163212351b479147d
hvhnguyen <hvhn3295@gmail.com>
2025-03-08 14:18:36 +0700

    Setup project with Spring Boot

---
+++ src/main/java/com/teppei/kendo/teppei_kendo_shop/TeppeiKendoShopApplication.java
@@ -0,0 +1,13 @@
+package com.teppei.kendo.teppei_kendo_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeppeiKendoShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeppeiKendoShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teppei/kendo/teppei_kendo_shop/TeppeiKendoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teppei.kendo.teppei_kendo_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeppeiKendoShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
