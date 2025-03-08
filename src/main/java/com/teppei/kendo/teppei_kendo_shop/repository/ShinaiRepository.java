60425fa227fadb7b6dd80d6882540bb5af40d519
hvhnguyen <hvhn3295@gmail.com>
2025-03-08 16:39:10 +0700

    feat(repository): add ShinaiRepository for data access

---
+++ src/main/java/com/teppei/kendo/teppei_kendo_shop/repository/ShinaiRepository.java
@@ -0,0 +1,7 @@
+package com.teppei.kendo.teppei_kendo_shop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.teppei.kendo.teppei_kendo_shop.model.Shinai;
+
+public interface ShinaiRepository extends JpaRepository<Shinai, Long>{
+}
